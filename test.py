d5f2f1cc1637d3830bf9060155c79c61f69474c0
massaoudiikram <82462579+massaoudiikram@users.noreply.github.com>
2021-07-24 01:56:13 +0100

    Update test.py

--- test.py
+++ test.py
@@ -5,5 +5,7 @@ response = requests.get(BASE + "trending_language")
 print(" number of languages :", response.json()['languages_count'])
 for i in range(len(response.json()['languages'])):
     print(response.json()['languages'][i], sep = "\n")
-
-
+    
+# your can get the results by running the scripts 
+# or you can 
+# run on your localhost : http://127.0.0.1:5000/trending_language
